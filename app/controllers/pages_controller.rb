c3523ac1ae6267e0c1d37617bfb1b6bdc3522ea9
Andres Gallardo <acgallard@gmail.com>
2017-08-27 04:17:01 -0300

    resultado

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,15 +8,21 @@ class PagesController < ApplicationController
     @hombres = params[:hombres].to_i
     @suma_invitados = @niños + @mujeres + @hombres
 
-    @carne = params[:carne]
-    @carne_total = @niños*0.2 + @mujeres*0.25 + @hombres*0.3
+    @calidad = params[:calidad]
+    @carne_total = @niños*0.2 + @mujeres*0.25 + @hombres*0.35
 
-    if @carne = "Lomo Vetado"
+    if @calidad = "tipo1"
+        @precio_carne = @carne_total*3800
+        @carne = "Chorizo San Jorge"
+    elsif @calidad = "tipo2"
+        @precio_carne = @carne_total*6400
+        @carne = "Sobrecostilla"
+    elsif @calidad = "tipo3"
         @precio_carne = @carne_total*9500
-    elsif @carne = "Lomo Liso"
-        @precio_carne = @carne_total*8500
-    elsif @carne = "Huachalomo"
-        @precio_carne = @carne_total*4500
+        @carne = "Lomo Vetado"
+    elsif @calidad = "tipo4"
+        @precio_carne = @carne_total*20000
+        @carne = "Wagyu"
     end
 
     @cerveza = params[:cerveza]
