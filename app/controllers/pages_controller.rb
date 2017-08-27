1db8cd326dc59cc942f47397a3943a5e5e156f49
Jjavier Silva <javier.eduardo.sp@gmail.com>
2017-08-26 22:24:17 -0300

    para agregar formato al form

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,29 @@
 class PagesController < ApplicationController
   def form
-
   end
 
   def calculo
-    @niños = params[:niños].to_i
+    @niños = params[:ninos].to_i
     @mujeres = params[:mujeres].to_i
-    @hombres = hombres[:hombres].to_i
+    @hombres = params[:hombres].to_i
     @suma_invitados = @niños + @mujeres + @hombres
+
+    @carne = params[:carne]
+    @carne_total = @niños*0.2 + @mujeres*0.25 + @hombres*0.3
+
+    if @carne = "Lomo Vetado"
+        @precio_carne = @carne_total*9500
+    elsif @carne = "Lomo Liso"
+        @precio_carne = @carne_total*8500
+    elsif @carne = "Huachalomo"
+        @precio_carne = @carne_total*4500
+    end
+
+    @cerveza = params[:cerveza]
+    @cerveza_total = @niños*0 + @mujeres*0.8 + @hombres*1.5
+
+
+
+
   end
 end
