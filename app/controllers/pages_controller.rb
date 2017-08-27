d17c5d3d8fdc68e6d9c62f990d0ca1ae2b3e213f
Jjavier Silva <javier.eduardo.sp@gmail.com>
2017-08-26 21:01:44 -0300

    formulario + CDN Bootstrap

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,12 @@
 class PagesController < ApplicationController
   def form
-    
+
   end
 
   def calculo
+    @niños = params[:niños].to_i
+    @mujeres = params[:mujeres].to_i
+    @hombres = hombres[:hombres].to_i
+    @suma_invitados = @niños + @mujeres + @hombres
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'pages/form'
   get 'pages/calculo'
   root 'pages#form'
+  #post 'pages', to: 'pages#calculo'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
