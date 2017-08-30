f0133d880c61ca95a47aaf7e3d1c6dc86d29b797
Juan Pablo Cuevas <jp.cuevaslavin@gmail.com>
2017-08-30 19:35:07 -0300

    Added app.js

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,70 +1,73 @@
+$(document).ready(function($) {
+	$('#btn_enviar_seleccion').on('click', function(){
+	 event.preventDefault();
+	 var hombres = parseInt($('#hombres').val());
+	 var mujeres = parseInt($('#mujeres').val());
+	 var ninos = parseInt($('#ninos').val());
+	 var calidad = $('#calidad').val();
+	 
+	 var total_invitados = hombres + mujeres + ninos;
+	 
+	 var carne_total = ninos*0.2 + mujeres*0.25 + hombres*0.35;
+	 var embutido_total = ninos*0.05 + mujeres*0.05 + hombres*0.1;
+	 var carbon = Math.round((carne_total + embutido_total)*3/5);
+	 var pan = Math.round((ninos*1 + mujeres*1 + hombres*2)/2);
 
-  def calculo
-    @calidad_hash = [["Beca JUNAEB","t1"], ["Yerno sin Aguinaldo","t2"], ["Recién Pagado","t3"], ["Alexis Sánchez","t4"]].to_h.invert
+	 switch (calidad) {
+	    case "t1":
+	        var precio_carne = carne_total*0;
+	        var carne = "";
+	        var embutido_total = embutido_total*2;
+	        carbon = Math.round(embutido_total*3/5);
+	        pan = pan*2;
+	        var precio_embutido = embutido_total*3800;
+	        var embutido = "Chorizo San Jorge";
+	        break;
+	    case "t2":
+	        var precio_carne = carne_total*6400;
+	        var carne = "Sobrecostilla";
+	        var precio_embutido = embutido_total*3800;
+	        var embutido = "Chorizo San Jorge";
+	        break;
+	    case "t3":
+	        var precio_carne = carne_total*9500;
+	        var carne = "Lomo Vetado";
+	        var precio_embutido = embutido_total*5800;
+	        var embutido = "Chorizo La Preferida";
+	        break;
+	    case "t4":
+	        var precio_carne = carne_total*20000;
+	        var carne = "Wagyu";
+	        var precio_embutido = embutido_total*7000;
+	        var embutido = "Longaniza Llanquihue";
+	        break;
+	  }
 
-    #Parámetros
-    @ninos = params[:ninos].to_i
-    @mujeres = params[:mujeres].to_i
-    @hombres = params[:hombres].to_i
-    @calidad = params[:calidad]
+	  if (carbon == 0) {carbon = 1};
 
-    #Calculos Personas
-    @suma_invitados = @ninos + @mujeres + @hombres
+	  var precio_total = precio_carne + precio_embutido + carbon * 2800 + pan/12 * 1200;
 
-    #Calculos Cantidad de Carnes
-    @carne_total = @ninos*0.2 + @mujeres*0.25 + @hombres*0.35
-    @embutido_total = @ninos*0.05 + @mujeres*0.05 + @hombres*0.1
-    @carbon = (@carne_total + @embutido_total)*3/5
-    @carbon = @carbon.round(0)
-    @pan = (@ninos*1 + @mujeres*1 + @hombres*2)/2.to_i
+	  var calidades = {
+	    t1: "Beca JUNAEB",
+	    t2: "Yerno sin Aguinaldo",
+	    t3: "Recién Pagado",
+	    t4: "Alexis Sánchez"
+	  }
 
-    #Asado según tipo 
-    if @calidad == "t1"
-        #Carne
-        @precio_carne = @carne_total*0
-        @carne = ""
-        #Embutido
-        @embutido_total = @embutido_total*2
-        @carbon = (@embutido_total*3/5).round(0)
-        @pan = @pan*2
-        @precio_embutido = @embutido_total*3800
-        @embutido = "Chorizo San Jorge"
+	  var tipo_asado = calidades[calidad]
 
-    elsif @calidad == "t2"
-        @precio_carne = @carne_total*6400
-        @carne = "Sobrecostilla"
-        @precio_embutido = @embutido_total*3800
-        @embutido = "Chorizo San Jorge"
+	  $('#invitados').html(total_invitados);
+	  $('#total-hombres').html(hombres);
+	  $('#total-mujeres').html(mujeres);
+	  $('#total-ninos').html(ninos);
+	  $('#tipo-asado').html(tipo_asado);
+	  $('#kilos-carne').html(carne_total);
+	  $('#kilos-embutido').html(embutido_total);
+	  $('#kilos-carbon').html(carbon);
+	  $('#unidades-pan').html(pan);
+	  $('#precio-total').html(precio_total);
 
-    elsif @calidad == "t3"
-        @precio_carne = @carne_total*9500
-        @carne = "Lomo Vetado"
-        @precio_embutido = @embutido_total*5800
-        @embutido = "Chorizo La Preferida"
+	  $('#resultado').removeAttr('hidden');
 
-    elsif @calidad == "t4"
-        @precio_carne = @carne_total*20000
-        @carne = "Wagyu"
-        @precio_embutido = @embutido_total*7000
-        @embutido = "Longaniza Llanquihue"
-
-    end
-
-    @precio_total = @precio_carne + @precio_embutido + @carbon * 2800 + @pan/12 * 1200 
-
-    if @carbon == 0 
-
-      @carbon = 1
-      
-    end
-
-
-
-    @cerveza = params[:cerveza]
-    @cerveza_total = @ninos*0 + @mujeres*0.8 + @hombres*1.5
-
-
-
-
-  end
-end
+	});
+});
--- javascripts/footer.js
+++ javascripts/footer.js
@@ -1,8 +1,8 @@
     $(document).ready(function() { 
-    var docHeight = $(window).height();
-    var footerHeight = $('#footer').height();
-    var footerTop = $('#footer').position().top + footerHeight;  
-    if (footerTop < docHeight) {
-    $('#footer').css('margin-top', 10 + (docHeight - footerTop) + 'px');
-    }
+	    var docHeight = $(window).height();
+	    var footerHeight = $('#footer').height();
+	    var footerTop = $('#footer').position().top + footerHeight;  
+	    if (footerTop < docHeight) {
+	    	$('#footer').css('margin-top', 10 + (docHeight - footerTop) + 'px');
+	    }
     });
